a5b0874864d8e700481ccfe109b0eb8363eeb5ee
dsalinda <dsalinda@virtusa.com>
2020-04-17 14:31:25 +0530

    add v

--- Codebases/src/main/java/App.java
+++ Codebases/src/main/java/App.java
@@ -5,5 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+		if("test"=="test"){
+			
+		}
     }
 }
